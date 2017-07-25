180dfac499d5aeea6c0d6a45866ea10c172c2aef
ajrominger <ajrominger@gmail.com>
2017-07-25 10:54:50 -0600

    figured out adjacency for d <= 2

--- prepData.R
+++ prepData.R
@@ -37,14 +37,71 @@ prepDataIsing <- function(path) {
                          cell = rep(rownames(LambdaMat), ncol(LambdaMat)), 
                          spin = as.vector(LambdaMat))
     
-    ## create edge list that indicates IDs of adjacent cells; use adjacent
+    ## create adjacency matrix that indicates adjacent cells
+    adj <- matrix(0, nrow = ncell(r), ncol = ncell(r))
+    offDiag <- row(adj) - col(adj)
+    
+    ## adj matrix for 1 neighbor is:
+    ## 1 in one off diag in each direction
+    adj[abs(offDiag) == 1] <- 1
+    
+    ## 0 in the one off diag for every [i*ncol, i*ncol + 1] | [i*ncol + 1, i*ncol] cells
+    adj[(offDiag == 1 & row(adj) %in% ((1:nrow(r)) * ncol(r) + 1)) | 
+            (offDiag == -1 & col(adj) %in% ((1:nrow(r)) * ncol(r) + 1))] <- 0
+    
+    ## 1 in ncol off diag in each direction
+    adj[abs(offDiag) == ncol(r)] <- 1
+
+    
+    foo <- graph_from_adjacency_matrix(adj)
+    plot(foo, layout = as.matrix(expand.grid(1:ncol(r), 1:nrow(r))), edge.arrow.size = 0.5)
+    
+    bla <- adj %*% adj + adj
+    bla[bla > 0] <- 1
+    which(bla[1, ] == 1)
+    
+    ego(foo, 2, nodes = 1)
+    
+    
+    r <- raster(nrows = 6, ncols = 8)
     edges <- adjacent(r, 1:ncell(r), directions = 4, sorted = TRUE)
+    edges <- edges[edges[, 2] - edges[, 1] != ncol(r) - 1, ]
+    laticeGraph <- graph_from_edgelist(edges)
+    write.table(as.matrix(as_adj(laticeGraph)), 'temp1.csv', sep = ',')
+    
+    par(mar = rep(1, 4))
+    image(1:ncol(r), 1:nrow(r), 
+          as.matrix(as_adj(laticeGraph)), axes = FALSE, col = c('white', 'black'))
+    
+    
+    plot(laticeGraph, layout = coordinates(r), edge.arrow.size = 1.5, 
+         edge.color = hsv(alpha = 0.3))
+    
+    
+    
+    
+    foo <- ego(laticeGraph, 2)
+    
+    foo[8, 1] <- 0
+    foo[16, 9] <- 0
+    foo[24, 17] <- 0
+    
+    goo <- foo %*% foo + foo
+    goo[goo > 0] <- 1
+    diag(goo) <- 0
+    par(mar = rep(0.1, 4))
+    plot(graph_from_adjacency_matrix(goo), layout = as.matrix(expand.grid(1:8, 1:3)), 
+         edge.arrow.size = 0.2, vertex.size = 0.5)
     
     ## calcualte correlation function for each spp: c(d) = < x_i * x_j > - < x >< x >
     bla <- traverceEL(edges, 2)
     head(bla[order(bla[, 1]), ])
     
 }
+
+prepDataIsing('../data/stri/UCSC.csv')
+
+
 
 ## function to traverse an edge list, returning all nodes within `d` links
 ## as applied to spatial occupancy
@@ -102,4 +159,3 @@ as_edgelist(graph_from_adj_list(ego(foo, 2)))
 
 
 
-prepDataIsing('../data/stri/UCSC.csv')
